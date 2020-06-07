99b46a860914bbb4dce4c71c575217717cf94e37
Flávia Schuenquer <flavia_schuenquer@hotmail.com>
2020-06-07 19:15:49 -0300

    first commit

---
+++ public/scripts/into.js
@@ -0,0 +1,64 @@
+//coments
+//documents.write("Hello")
+
+//variávies, tipos de dados
+//var myvar
+//myvar= "He"
+//cosnt myconst = "He"
+
+//documents.write(myconst + myvar)
+
+//string
+//"Isso é uma string"
+//'Isto é uma string'
+//*Isto é uma string*
+
+//number
+//const n1 = 1
+//const n2 = 12
+//document.write(ni+n2)
+
+//boolean
+//const bTrue = true
+//cosnt bFalse = false
+//documents.write(bFalse)
+
+//objeto possuem propriedade e funcionalidade
+//cosnt pessoa = {
+//altura = 1,80m,
+//idade = 24,
+//solteiro = true  
+//correr() {
+  //documents.write("executar uma grande lógica de correr")
+//}
+//}
+//pessoa.correr()
+
+//funçoes - executa, atalhos
+//exemplo
+
+//Array - Vetores [...]
+
+//cosnt colecaoDeBolinhas = ["blue", "green", 1, {Name = "My name"}]
+//documents.write(colecaoDeBolinhas [3].name)
+
+
+
+//function sayMyName(name) {
+ // document.write("name")
+//}
+//sayMyName("Flávia")
+
+//condicionais
+
+//cosnt notaFinal = 7
+//if(notaFinal < 5) {
+//document.write("Reprovado")
+// else
+//document.write("Aprovado")
+//}
+
+//loop repetições
+//for(i=0, i < 10, i++) {
+//document.write('<p>${i}</p>')
+//}
